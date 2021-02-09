896fb2610a8a16060e96f27ffc1f1706c4cc7604
Thomas Varsamidis <tvarsamidis@codehub.gr>
2021-02-09 03:22:59 +0200

    first 2

--- src/a/b/c/Test.java
+++ src/a/b/c/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("first 2");
         System.out.println("second");
+        System.out.println("Keep this important line");
     }
 }
