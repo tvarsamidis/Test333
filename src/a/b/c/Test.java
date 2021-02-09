4df0e99a00a3975d9c5a7b54ae2cbfba10e12241
Thomas Varsamidis <tvarsamidis@codehub.gr>
2021-02-09 03:16:48 +0200

    second

--- src/a/b/c/Test.java
+++ src/a/b/c/Test.java
@@ -2,8 +2,7 @@ package a.b.c;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("first");
-        System.out.println("second");
         System.out.println("first 2");
+        System.out.println("second");
     }
 }
